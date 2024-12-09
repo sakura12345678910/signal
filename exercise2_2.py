bba18a9e4dd18c99ecef6ac7b366d8dc7f914809
mori nana <nana@morinana.com>
2024-12-09 11:47:00 +0900

    add exercise2_2.py

--- exercise2_2.py
+++ exercise2_2.py
@@ -1,4 +1,3 @@
-```python
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -27,4 +26,3 @@ t = np.linspace(-PI, PI, 1000)
 plt.plot(t, npclk(t))
 plt.plot(t, partialsum(t, 5))
 plt.show()
-```
